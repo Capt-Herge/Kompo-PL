4e8e40b49110b148650ee8cfe74e4138faf5e711
Plasmatikus <jo.schuhn@googlemail.com>
2018-07-25 11:42:30 +0200

    Nachtrag zu umbau von CompPartsLogic

---
+++ CompPartsLogic/CLogicAdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsLogic
+{
+    internal class CLogicAdd : ILogicAdd
+    {
+        #region fields
+        private IDataAdd _dataAdd;
+        #endregion
+
+        #region ctor
+        internal CLogicAdd(IDataAdd dataAdd)
+        {
+            _dataAdd = dataAdd;
+        }
+        #endregion
+    }
+}
---
+++ CompPartsLogic/Factories/AFactoryLogicAdd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsLogic.Factories
+{
+    public abstract class AFactoryLogicAdd
+    {
+        public static void Create(ILogic logic, IDataAdd dataAdd)
+        {
+
+        }
+    }
+}
