370263160a2fd2bcc04062778f21b970e2a2ae69
Capt-Herge <capt_herge@freenet.de>
2018-07-25 18:56:55 +0200

    Abstrakte Klasse in PartsData bearbeitet

--- CompPartsData/Access/CDataSearch.cs
+++ CompPartsData/Access/CDataSearch.cs
@@ -9,5 +9,15 @@ namespace PartsData.Access
 {
     class CDataSearch : ADataSearch
     {
+        #region ctor
+        internal CDataSearch(AData data) : base(data)
+        {
+
+        }
+        #endregion
+
+        #region protected methods override
+
+        #endregion
     }
 }
