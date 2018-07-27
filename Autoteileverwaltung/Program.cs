88927a35c9228f382ad6064b2615e2b96ee1d57f
Plasmatikus <jo.schuhn@googlemail.com>
2018-07-27 13:35:17 +0200

    Implementieren des Program.cs fürs Testing

--- Autoteileverwaltung/Program.cs
+++ Autoteileverwaltung/Program.cs
@@ -22,8 +22,33 @@ namespace Autoteileverwaltung
         #region methods
         void Run()
         {
+            //Connection String zum MSSQL 2016 Server (Standartinstand, Standart-Auth)
+            //Server bei Johannes: Server=plasma.selfhost.bz;Database=PartsData;User Id=sa;Password=AngewandteInformatik1337!;
+            string connectionString = "Server=192.168.147.3;Database=PartsData;User Id=sa;Password=AngewandteInformatik1337!;";
+
+            //Erstellung - PartsData
+            IFactoryIData factoryData = new CFactoryCDataAccess();
+            _data = factoryData.Create(connectionString);
+
+            //Erstellung - PartsLogic
             IFactoryILogic factoryLogic = new CFactoryCLogic();
+            _logic = factoryLogic.Create(_data);
+            AFactoryLogicAdd.Create(_logic, _data.Add);
+            AFactoryLogicModify.Create(_logic, _data.Modify);
+            AFactoryLogicSearch.Create(_logic, _data.Search);
+
+            //Erstelllung - PartsDialog
             IFactoryIDialog factoryDialog = new CFactoryCDialog();
+            _dialogMain = factoryDialog.Create(_logic);
+            //AFactoryDialogAdd.CreateAdd(_logic.Add, _dialogMain);
+            //AFactoryDialogModify.CreateModify(_logic.Modify, _dialogMain);
+            AFactoryDialogSearch.CreateSearch(_logic.Search, _dialogMain);
+
+            //Initialisieren der Datenbankverbindung
+            _data.Init();
+
+            //Initialisierung fertig - Starten des Hauptdialogs
+            Application.Run(_dialogMain as Form);
         }
         /// <summary>
         /// The main entry point for the application.
